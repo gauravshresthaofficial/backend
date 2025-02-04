400c7df43fcf12d63e30c76e520ac243249524a5
Gaurav <imgauravshrestha@gmail.com>
2025-02-04 09:32:45 +0545

    Phonebook_backend

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+
+morgan.token("post-body", (req) => {
+  return req.method === "POST" ? JSON.stringify(req.body) : "";
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :post-body"
+  )
+);
 
 let persons = [
   {
